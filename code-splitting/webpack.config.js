9c4923d75423a6601c92b5901ccdf9512a935685
soonwoo <sw5063@naver.com>
2022-11-16 23:30:35 +0900

    css 분리 loader 작성

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -1,18 +1,38 @@
 var path = require('path');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'none',
+  mode: 'none', // production -> 실무, development -> 개발, none
   entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  //**css 분리 x */
+  //loader
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       //순서 지켜야함
+  //       use: ['style-loader', 'css-loader']
+  //     }
+  //     //scss
+  //     // {
+  //     //   test: /\.scss$/,
+  //     //   use: ['style-loader', 'css-loader','sass-loader']
+  //     // }
+  //   ]
+  // }
+
+  /**css 분리 loader 작성 */
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [new MiniCssExtractPlugin()]
 };
